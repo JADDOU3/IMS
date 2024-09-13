9098591b1a1545ee74d48f5be7d34026bfe88df3
JADDOU3 <22the7@gmail.com>
2024-09-13 18:23:07 +0300

    commit

--- src/main/java/org/example/controller/managecontact/UpdateContact.java
+++ src/main/java/org/example/controller/managecontact/UpdateContact.java
@@ -16,27 +16,71 @@ public class UpdateContact implements State {
     }
     @Override
     public void handleInput() {
-        Contact contact = null;
         System.out.println("Choose the Contact type :\n"+
                 "1. Supplier     2. Buyer");
-
-        String type;
         switch (context.getScanner().nextLine()){
             case "1":
-                contact = new Supplier();
+                suppHelper();
                 break;
             case "2"  :
-                contact = new Buyer();
                 break;
             default:
                 System.out.println("Please enter a valid Contact Type");
                 handleInput();
         }
+
+    }
+
+    private void suppHelper(){
+        Supplier supplier = new Supplier();
         System.out.println("Enter the Contact ID");
         int id = context.getScanner().nextInt();
-        contact.setId(id);
+        supplier.setId(id);
+        System.out.println("Choose what you want to update\n"+
+                "1. Name        2. Phone\n"+
+                "3. Email       4. Address\n"+
+                "5. Rating      6. prefabrication\n" +
+                "7. Back");
+        switch (context.getScanner().nextInt()){
+            case 1:
+                System.out.println("Enter the new Name");
+                supplier.setName(context.getScanner().nextLine());
+                break;
+            case 2:
+                System.out.println("Enter the new Phone");
+                supplier.setPhone(context.getScanner().nextLine());
+                break;
+            case 3:
+                System.out.println("Enter the new Email");
+                supplier.setEmail(context.getScanner().nextLine());
+                break;
+            case 4:
+                System.out.println("Enter the new Address");
+                supplier.setAddress(context.getScanner().nextLine());
+                break;
+            case 5:
+                System.out.println("Enter the new Rating");
+                int rating = context.getScanner().nextInt();
+                if(rating > 100 || rating < 0)
+                    supplier.setRating(rating);
+                else
+                    System.out.println("Invalid Rating ; Should be Between 0 and 100");
+                break;
+            case 6:
+                boolean isPreferred;
 
-
+                //supplier.setPreferred(isPreferred);
+                break;
+            case 7:
+                handleInput();
+                break;
+            default:
+                System.out.println("Invalid Input");
+                suppHelper();
+                break;
+        }
+        updateContact(supplier);
+        updateSupplier(supplier);
     }
 
 
