e366f39919b9a0aa80af484d670c0f819d4384aa
JADDOU3 <22the7@gmail.com>
2024-09-23 19:30:35 +0300

    add via csv

--- src/main/java/org/example/controller/manageitem/AddCSV.java
+++ src/main/java/org/example/controller/manageitem/AddCSV.java
@@ -4,6 +4,10 @@ import org.example.controller.Context;
 import org.example.controller.State;
 import org.example.item.Item;
 
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -18,6 +22,45 @@ public class AddCSV implements State {
 
     @Override
     public void handleInput() {
+        System.out.println("Enter the file path :");
+        String in = context.getScanner().nextLine();
+        addFromCSV(in);
+
+        context.setCurrentState(new ManageItem(context));
+        context.handleInput();
+    }
+
+    public void addFromCSV(String filePath){
+        String querySQL = "INSERT INTO items (item_name, quantity, price) VALUES (?, ?, ?)";
+        try(Connection connection = DriverManager.getConnection(context.getDatabaseInfo()[0],context.getDatabaseInfo()[1],context.getDatabaseInfo()[2]);
+            PreparedStatement preparedStatement = connection.prepareStatement(querySQL);
+            BufferedReader br = Files.newBufferedReader(Path.of(filePath))){
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+
+                if (values.length == 3) {
+                    String itemName = values[0];
+                    int quantity = Integer.parseInt(values[1]);
+                    int reorderLevel = Integer.parseInt(values[2]);
+
+                    preparedStatement.setString(1, itemName);
+                    preparedStatement.setInt(2, quantity);
+                    preparedStatement.setInt(3, reorderLevel);
+                    preparedStatement.addBatch();
+                } else {
+                    System.out.println("Invalid data format in CSV.");
+                }
+            }
+
+            preparedStatement.executeBatch();
+            System.out.println("items added successfully!");
+
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
